0f536a10f2b2ac033b960fb55d825b4eb8b8841e
Ahmed Swerky <ahmed.swerky@givingloop.org>
2021-09-04 03:42:35 +0200

    wip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld :msg="`${greeting} ${user.name}`" />
+    <button @click="changeGreeting">Greet Again</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { reactive, ref } from "@vue/reactivity";
+import HelloWorld from "@/components/HelloWorld";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+  },
+  setup() {
+    const data = {
+      greeting: ref(`Welcome to Your Vue.js App`),
+      user: reactive({
+        name: "John Doe",
+        email: "john@doe.me",
+      }),
+    };
+
+    const methods = {
+      changeGreeting: () => {
+        data.greeting.value = `Welcome again`;
+      },
+    };
+
+    return {
+      ...data,
+      ...methods,
+    };
   },
 };
 </script>
